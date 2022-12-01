c746fe17c062bf6de47dc1c778f76c541400b152
Riccardo Binetti <rbino@gmx.com>
2022-12-01 18:53:09 +0100

    Solve day1, part2
    Remove unused utils functions
    
    Signed-off-by: Riccardo Binetti <rbino@gmx.com>

--- src/day01.zig
+++ src/day01.zig
@@ -8,11 +8,22 @@ const BitSet = std.DynamicBitSet;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
+// Useful stdlib functions
+const tokenize = std.mem.tokenize;
+const split = std.mem.split;
+const swap = std.mem.swap;
+const parseInt = std.fmt.parseInt;
+const print = std.debug.print;
+const assert = std.debug.assert;
+
 const data = @embedFile("data/day01.txt");
 
 pub fn main() !void {
-    const result = try solvePart1(data);
-    print("Part 1 result: {}\n", .{result});
+    const part1_result = try solvePart1(data);
+    print("Part 1 result: {}\n", .{part1_result});
+
+    const part2_result = try solvePart2(data);
+    print("Part 2 result: {}\n", .{part2_result});
 }
 
 fn solvePart1(input: []const u8) !usize {
@@ -30,6 +41,37 @@ fn solvePart1(input: []const u8) !usize {
         }
     }
     return max_calories;
+}
+
+fn solvePart2(input: []const u8) !usize {
+    // The top 3 calories transporters, in descending order of calories
+    var top_3_calories_desc: [3]usize = .{0} ** 3;
+    var elf_iterator = split(u8, input, "\n\n");
+    while (elf_iterator.next()) |elf| {
+        var elf_calories: usize = 0;
+        var food_iterator = tokenize(u8, elf, "\n");
+        while (food_iterator.next()) |food| {
+            const calories = try parseInt(usize, food, 10);
+            elf_calories += calories;
+        }
+        var i: u8 = 0;
+        while (i < top_3_calories_desc.len) : (i += 1) {
+            if (elf_calories > top_3_calories_desc[i]) {
+                var j: u8 = top_3_calories_desc.len - 1;
+                while (j > i) : (j -= 1) {
+                    swap(usize, &top_3_calories_desc[j], &top_3_calories_desc[j - 1]);
+                }
+                top_3_calories_desc[i] = elf_calories;
+                break;
+            }
+        }
+    }
+
+    var sum: usize = 0;
+    for (top_3_calories_desc) |calories| {
+        sum += calories;
+    }
+    return sum;
 }
 
 test "example input" {
@@ -50,38 +92,10 @@ test "example input" {
         \\10000
     ;
 
-    const result = try solvePart1(input);
-    assert(result == 24000);
+    const part1_result = try solvePart1(input);
+    assert(part1_result == 24000);
+
+    const part2_result = try solvePart2(input);
+    assert(part2_result == 45000);
 }
 
-// Useful stdlib functions
-const tokenize = std.mem.tokenize;
-const split = std.mem.split;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
-const min = std.math.min;
-const min3 = std.math.min3;
-const max = std.math.max;
-const max3 = std.math.max3;
-
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.sort;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.

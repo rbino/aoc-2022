8c3cf208171296fcdeb8ab79be5424dca607c1b3
Riccardo Binetti <rbino@gmx.com>
2022-12-03 02:00:31 +0100

    day02: solve part 1

--- src/day02.zig
+++ src/day02.zig
@@ -4,14 +4,73 @@ const List = std.ArrayList;
 const Map = std.AutoHashMap;
 const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
+const Order = std.math.Order;
 
 const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day02.txt");
 
+pub const Shape = enum(u8) {
+    rock = 1,
+    paper = 2,
+    scissors = 3,
+
+    const loss_points: usize = 0;
+    const draw_points: usize = 3;
+    const victory_points: usize = 6;
+
+    pub fn parse(in: u8) Shape {
+        return switch (in) {
+            'A', 'X' => .rock,
+            'B', 'Y' => .paper,
+            'C', 'Z' => .scissors,
+            else => unreachable,
+        };
+    }
+
+    pub fn shapePoints(self: Shape) usize {
+        return @enumToInt(self);
+    }
+
+    pub fn outcomePoints(self: Shape, other: Shape) usize {
+        if (self == other) return draw_points;
+
+        return switch (self) {
+            .rock => if (other == .scissors) victory_points else loss_points,
+            .paper => if (other == .rock) victory_points else loss_points,
+            .scissors => if (other == .paper) victory_points else loss_points,
+        };
+    }
+};
+
 pub fn main() !void {
-    
+    const part1_result = try solvePart1(data);
+    print("Part 1 result: {}\n", .{part1_result});
+}
+
+fn solvePart1(input: []const u8) !usize {
+    var total_score: usize = 0;
+    var lines = tokenize(u8, input, "\n");
+    while (lines.next()) |line| {
+        assert(line.len == 3);
+        const elf_shape = Shape.parse(line[0]);
+        const player_shape = Shape.parse(line[2]);
+        total_score += player_shape.outcomePoints(elf_shape) + player_shape.shapePoints();
+    }
+
+    return total_score;
+}
+
+test "example input" {
+    const input =
+        \\A Y
+        \\B X
+        \\C Z
+    ;
+
+    const part1_result = try solvePart1(input);
+    assert(part1_result == 15);
 }
 
 // Useful stdlib functions

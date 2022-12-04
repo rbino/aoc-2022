c64c3a513b3c9a067fa18ff6f732a06ee4883b52
Riccardo Binetti <rbino@gmx.com>
2022-12-04 11:47:47 +0100

    day04: solve part1

--- src/day04.zig
+++ src/day04.zig
@@ -11,9 +11,53 @@ const gpa = util.gpa;
 const data = @embedFile("data/day04.txt");
 
 pub fn main() !void {
-    
+    const part1_result = try solvePart1(data);
+    print("Part 1 result: {}\n", .{part1_result});
 }
 
+fn solvePart1(input: []const u8) !usize {
+    var fully_contained: usize = 0;
+    var lines = tokenize(u8, input, "\n");
+    while (lines.next()) |line| {
+        const comma_idx = indexOf(u8, line, ',').?;
+        const first_range = try Range.parse(line[0..comma_idx]);
+        const second_range = try Range.parse(line[comma_idx + 1 ..]);
+        if (first_range.fullyContains(second_range) or second_range.fullyContains(first_range)) {
+            fully_contained += 1;
+        }
+    }
+    return fully_contained;
+}
+
+pub const Range = struct {
+    start: usize,
+    end: usize,
+
+    pub fn parse(section: []const u8) !Range {
+        const dash_idx = indexOf(u8, section, '-').?;
+        return Range{
+            .start = try parseInt(usize, section[0..dash_idx], 10),
+            .end = try parseInt(usize, section[dash_idx + 1 ..], 10),
+        };
+    }
+
+    pub fn fullyContains(self: Range, other: Range) bool {
+        return (other.start >= self.start and other.end <= self.end);
+    }
+};
+
+test "example input" {
+    const input =
+        \\2-4,6-8
+        \\2-3,4-5
+        \\5-7,7-9
+        \\2-8,3-7
+        \\6-6,4-6
+        \\2-6,4-8
+    ;
+
+    assert(try solvePart1(input) == 2);
+}
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
 const split = std.mem.split;

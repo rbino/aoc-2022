b80d02d32849b75a36ee94b34986079e0bc890fd
Riccardo Binetti <rbino@gmx.com>
2022-12-07 15:29:43 +0100

    day06: solve part1

--- src/day06.zig
+++ src/day06.zig
@@ -11,7 +11,49 @@ const gpa = util.gpa;
 const data = @embedFile("data/day06.txt");
 
 pub fn main() !void {
-    
+    const part1_result = solvePart1(data);
+    print("Part 1 result: {}\n", .{part1_result});
+}
+
+fn solvePart1(input: []const u8) usize {
+    var detector = MarkerDetector{};
+    for (trimRight(u8, input, "\n")) |c| {
+        if (detector.feedAndDetect(c)) {
+            return detector.pos;
+        }
+    }
+    unreachable;
+}
+
+pub const MarkerDetector = struct {
+    const marker_length = 4;
+    const Self = @This();
+
+    buffer: [marker_length]u8 = undefined,
+    pos: usize = 0,
+    start: u2 = 0,
+    end: u2 = 0,
+
+    pub fn feedAndDetect(self: *Self, char: u8) bool {
+        var i: u2 = self.start;
+        while (i != self.end) : (i +%= 1) {
+            if (self.buffer[i] == char) {
+                self.start = i +% 1;
+            }
+        }
+        self.buffer[self.end] = char;
+        self.end +%= 1;
+        self.pos += 1;
+        return self.start == self.end;
+    }
+};
+
+test "example input" {
+    assert(solvePart1("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7);
+    assert(solvePart1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5);
+    assert(solvePart1("nppdvjthqldpwncqszvftbrmjlhg") == 6);
+    assert(solvePart1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10);
+    assert(solvePart1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11);
 }
 
 // Useful stdlib functions
@@ -23,7 +65,7 @@ const indexOfStr = std.mem.indexOfPosLinear;
 const lastIndexOf = std.mem.lastIndexOfScalar;
 const lastIndexOfAny = std.mem.lastIndexOfAny;
 const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
+const trimRight = std.mem.trimRight;
 const sliceMin = std.mem.min;
 const sliceMax = std.mem.max;
 
